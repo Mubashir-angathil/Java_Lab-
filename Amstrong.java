c582026c2f13443f5c769fb53cd0e0d216e72f3a
Mubashir Angathil <mubashirangathil5142@gmail.com>
2021-10-11 20:41:47 +0530

    Amstrong is completed

---
+++ Amstrong.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.lang.Math;
+
+class Calc{
+
+     int count(int num){
+     int count=0;
+     while(num!=0)
+     {
+         count++;
+        num = num/10;}
+                          
+     {
+     }
+     return count;
+     }
+
+    
+    void calc(int start ,int end){ 
+    
+        int i,j;
+    for(i=start;i<end;i++)
+    {
+        int sum = 0;
+        int a = i;
+        int b = i;
+
+        while(a>0)
+        {
+            j = a % 10;
+            sum += (Math.pow(j,count(i)));
+            a = a/10;
+        }
+        if (sum==b)
+         {
+            System.out.println(sum);
+        }
+
+    }
+   }
+}
+public class Amstrong{
+    public static void main (String[] args)throws Exception{
+    
+    Calc c = new Calc();
+    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println("Enter a range: ");
+    
+    System.out.print("Begin : ");
+    int start = Integer.parseInt( bf.readLine());
+    System.out.print("End : ");
+    int end = Integer.parseInt(bf.readLine());
+    System.out.println("Amstrong numbers are :");
+    c.calc(start,end);
+  
+   }
+}
