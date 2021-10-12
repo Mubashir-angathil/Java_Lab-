f0bfa1ee684ae6aba0125f53c4ea577bea71a198
Mubashir Angathil <mubashirangathil5142@gmail.com>
2021-10-12 23:11:32 +0530

    Remove all vowels from a string is completed by using replace method

--- Vowel.java
+++ Vowel.java
@@ -1,37 +1,16 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.List;
-
-
-
-public class Vowel
-{
-	  static String remVowel(String str)
-	{
-		Character vowels[] = {'a', 'e', 'i', 'o', 'u','A','E','I','O','U'};
-		
-		List<Character> al = Arrays.asList(vowels);
-		
-		StringBuffer sb = new StringBuffer(str);
-		
-		for (int i = 0; i < sb.length(); i++) {
-			
-			if(al.contains(sb.charAt(i))){
-				sb.replace(i, i+1, "") ;
-				i--;
-			}
-		}
-		
-		
-		return sb.toString();
-	}
-	// Driver method to test the above function
-	public static void main(String[] args)throws Exception
-	{
-      BufferedReader bf= new BufferedReader(new InputStreamReader(System.in)) ;
-      System.out.print("Enter a string : ");
-		String str =bf.readLine(); 
-		System.out.print("The string after removing vowels : ");
-		System.out.println(remVowel(str));
-	}
+// Java Program to Delete Vowels from String
+public class Vowel {
+    public static void main(String args[])throws Exception {
+        String str, output;
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+ 
+        System.out.print("Enter a String : ");
+        str = bf.readLine();
+         // Deleting Vowel alphabets from input
+        // string using replaceAll method
+        output = str.replaceAll("[AEIOUaeiou]", "");
+ 
+        System.out.println("Output String\n" + output);
+    }
 }
