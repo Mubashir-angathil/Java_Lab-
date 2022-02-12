851e037037d33aed46baf7a3a16a41c48d01406a
Mubashir Angathil <mubashirangathil5142@gmail.com>
2022-02-13 01:04:53 +0530

    nineteen programs completed

---
+++ CountObjects.java
@@ -0,0 +1,25 @@
+    public class CountObjects{
+
+        // static varibale to store number of objects
+        static int count = 0;
+    
+        // various types of constructor
+        public CountObjects(){
+            count+=1;
+        }
+    
+        public CountObjects(int i){
+            count+=1;
+        }
+        
+        public static void main(String[] args) {
+            CountObjects obj1 = new CountObjects();
+            CountObjects obj2 = new CountObjects();
+            CountObjects obj3 = new CountObjects();
+            CountObjects obj4 = new CountObjects();
+    
+            // display count
+            System.out.println("Number of objects created : " + CountObjects.count);
+        }
+    }    
+
