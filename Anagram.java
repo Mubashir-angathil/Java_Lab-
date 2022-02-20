b1f3d13166070f96344ff095689cd269f1362763
Mubashir Angathil <mubashirangathil5142@gmail.com>
2022-02-20 15:19:31 +0530

    Anagram program error fixed

--- Anagram.java
+++ Anagram.java
@@ -1,56 +1,62 @@
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Anagram {
+    static String strTwo;
 
-    /*
-     * function to check whether two strings are
-     * anagram of each other
-     */
-    static boolean areAnagram(char[] str1, char[] str2) {
-        // Get lengths of both strings
-        int n1 = str1.length;
-        int n2 = str2.length;
-
-        // If length of both strings is not same,
-        // then they cannot be anagram
-        if (n1 != n2)
-            return false;
-
-        // Sort both strings
-        Arrays.sort(str1);
-        Arrays.sort(str2);
-
-        // Compare sorted strings
-        for (int i = 0; i < n1; i++)
-            if (str1[i] != str2[i])
-                return false;
-
-        return true;
-    }
-
-    public static void sortStr2(char[] str1, char[] str2) {
-        for(int i=0;i<4;i++){
-
-            System.out.println(str1[i]+str2[i]);
+    public static void arrangeStrting(String format) {
+        int i, j;
+        char[] temp1 = new char[20];
+        for (i = 0; i < format.length(); i++) {
+            for (j = 0; j < strTwo.length(); j++) {
+                if (format.charAt(i) == strTwo.charAt(j)) {
+                    temp1[i] = strTwo.charAt(j);
+                    break;
+                }
+            }
+        }
+        for (i = 0; i < 5; i++) {
+            System.out.print(temp1[i]);
         }
     }
 
-    /* Driver Code */
-    public static void main(String args[]) {
-        char str1[] = { 't', 'e', 's', 't' };
-        char str2[] = { 't', 's', 'e', 't' };
-        
-        // Function Call
-        if (areAnagram(str1, str2)) {
+    public static void main(String[] args) {
+        String strOne, Format;
+        int lenOne, lenTwo, i, j, found = 0, not_found = 0;
+        Scanner scan = new Scanner(System.in);
 
-            System.out.println("The two strings are anagram of each other: "+str1[3] );
-            sortStr2(str1,str2);
-        } else {
+        System.out.print("Enter the First String: ");
+        strOne = scan.nextLine();
+        strOne=strOne.toUpperCase();
+        Format = strOne;
+        System.out.print("Enter the Second String: ");
+        strTwo = scan.nextLine();
+        strTwo=strTwo.toUpperCase();
+        lenOne = strOne.length();
+        lenTwo = strTwo.length();
 
-            System.out.println("The two strings are not anagram of each other");
+        if (lenOne == lenTwo) {
+            for (i = 0; i < lenOne; i++) {
+                found = 0;
+                for (j = 0; j < lenOne; j++) {
+                    if (strOne.charAt(i) == strTwo.charAt(j)) {
+                        found = 1;
+                        break;
+                    }
+                }
+                if (found == 0) {
+                    not_found = 1;
+                    break;
+                }
+            }
+            if (not_found == 1)
+                System.out.println("\nStrings are not Anagram");
+            else {
+                System.out.println("\nStrings are Anagram");
+                arrangeStrting(Format);
+            }
         }
 
+        else
+            System.out.println("\nLength of Strings Mismatched!");
     }
 }
-
-// This code is contributed by Nikita Tiwari.
